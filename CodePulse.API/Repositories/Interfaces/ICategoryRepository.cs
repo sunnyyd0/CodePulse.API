b790dac8571bc523222bcd0e7a402b18b8084b10
sunny yadav <sunny.in645@gmail.com>
2025-10-25 10:09:00 +0530

    Add project files.

---
+++ CodePulse.API/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using CodePulse.API.Models.Domain;
+
+namespace CodePulse.API.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateAsync(Category category);
+    }
+}
